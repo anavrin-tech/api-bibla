79b6107261ff8974cf8a7f4f219896a6d2a5af3e
anavrin-tech <david.borges@anavrin.tech>
2021-01-23 22:10:15 -0300

    sprint3

--- main.py
+++ main.py
@@ -35,24 +35,40 @@ async def index():
     }
 
 
-@app.get('/nvi', tags=["Versão: NVI"])
-async def biblia_nvi(book_id: int = None, chapter: int = None, verse: int = None, db: Session = Depends(get_db), offset: int = 0, limit: int = 100):
+@app.get('/nvi/books', tags=["Books - Versão NVI"])
+async def books(db: Session = Depends(get_db)):
+    books = db.query(models.Books)
+    return books.all()
+
+
+@app.get('/nvi/chapter', tags=["Chapter - Versão NVI"])
+async def chapter(book_id: int = None, db: Session = Depends(get_db), offset: int = 0, limit: int = 100):
+    chapter = db.query(models.Nvi.chapter).distinct(models.Nvi.chapter)
+
+    if book_id:
+        chapter = chapter.filter(models.Nvi.book_id == book_id)
+
+    chapter = chapter.offset(offset).limit(limit).all()
+
+    if chapter == []:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return chapter
+
+
+@app.get('/nvi', tags=["Text - Versão NVI"])
+async def text(book_id: int = None, chapter: int = None, db: Session = Depends(get_db), offset: int = 0, limit: int = 100):
     '''
         Atributos passado pelo Path:
         book_id,
         chapter,
-        verse
     '''
-    nvi = db.query(models.Nvi)
+    nvi = db.query(models.Nvi.text)
 
     if book_id:
-        nvi = nvi.filter(models.Nvi.book_id >= book_id)
+        nvi = nvi.filter(models.Nvi.book_id == book_id)
 
     if chapter:
-        nvi = nvi.filter(models.Nvi.chapter >= chapter)
-
-    if verse:
-        nvi = nvi.filter(models.Nvi.verse >= verse)
+        nvi = nvi.filter(models.Nvi.chapter == chapter)
 
     nvi = nvi.offset(offset).limit(limit).all()
 
@@ -61,19 +77,54 @@ async def biblia_nvi(book_id: int = None, chapter: int = None, verse: int = None
     return nvi
 
 
-@app.get('/acf', tags=["Versão: ACF"])
-async def biblia_acf(book_id: int = None, chapter: int = None, verse: int = None, db: Session = Depends(get_db), offset: int = 0, limit: int = 100):
+@app.get('/acf/books', tags=["Books - Versão ACF"])
+async def books(db: Session = Depends(get_db)):
+    books = db.query(models.Books)
+    return books.all()
 
-    acf = db.query(models.Acf)
+
+@app.get('/acf/chapter', tags=["Chapter - Versão ACF"])
+async def chapter(book_id: int = None, db: Session = Depends(get_db), offset: int = 0, limit: int = 100):
+    chapter = db.query(models.Nvi.chapter).distinct(models.Nvi.chapter)
 
     if book_id:
-        acf = acf.filter(models.Acf.book_id == book_id)
+        chapter = chapter.filter(models.Nvi.book_id == book_id)
+
+    chapter = chapter.offset(offset).limit(limit).all()
+
+    if chapter == []:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return chapter
+
+
+@app.get('/acf', tags=["Text - Versão ACF"])
+async def text(book_id: int = None, chapter: int = None, db: Session = Depends(get_db), offset: int = 0, limit: int = 100):
+
+    nvi = db.query(models.Nvi.text)
+
+    if book_id:
+        nvi = nvi.filter(models.Nvi.book_id == book_id)
 
     if chapter:
-        acf = acf.filter(models.Acf.chapter == chapter)
+        nvi = nvi.filter(models.Nvi.chapter == chapter)
 
-    if verse:
-        acf = acf.filter(models.Acf.verse == verse)
+    nvi = nvi.offset(offset).limit(limit).all()
 
-    acf = acf.offset(offset).limit(limit).all()
-    return acf
+    if nvi == []:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return nvi
+
+""" 
+@app.get('/search', tags=["Mecanismo de Busca"])
+async def search_word(search: str = None, db: Session = Depends(get_db), offset: int = 0, limit: int = 1000):
+
+    busca = db.query(models.Nvi)
+
+    if search:
+        busca = busca.filter(models.Nvi.text.like('% '+search+' %'))
+        if busca == []:
+            return {'message': '{} not found!'.format(search)}
+        busca = busca.offset(offset).limit(limit).all()
+        return busca
+    return{'message': 'word search not passed to parameter.'}
+ """
